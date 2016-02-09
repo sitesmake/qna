ffad75fc8866b8d5d4a2d7bfa8a1eb11b5d1f240
Sites Make <sitesmake@gmail.com>
2016-02-10 02:14:11 +0300

    js destroy question

---
+++ spec/acceptance/delete_answers_spec.rb
@@ -0,0 +1,39 @@
+require_relative 'acceptance_helper'
+
+feature 'User delete answer', %q{
+  In order to delete answer
+  As an authenticated user
+  I want to be able to delete answer
+} do
+
+  given(:user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+
+  scenario 'Authenticated user deletes question', js: true do
+  	sign_in user
+
+    visit question_path(question)
+
+    click_on '(delete)'
+
+    expect(current_path).to eq questions_path
+    expect(page).to_not have_content question.title
+    expect(page).to_not have_content question.body
+  end
+
+  scenario 'Other user do not see delete question link' do
+    other_user = create(:user)
+    sign_in other_user
+
+	  visit question_path(question)
+
+	  expect(current_path).to_not have_link '(delete)'
+  end
+
+  scenario 'Guest do not see delete question link' do
+	  visit question_path(question)
+
+	  expect(current_path).to_not have_link '(delete)'
+  end
+
+end
