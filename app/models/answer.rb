08f58e2a11bb08a5368946a7bc6a95b6f4d6c84f
Sites Make <sitesmake@gmail.com>
2016-02-13 09:25:19 +0300

    best answer in Answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,13 @@ class Answer < ActiveRecord::Base
   validates :body, presence: true
   validates :question_id, numericality: true, presence: true
   validates :user_id, numericality: true, presence: true
+
+  def best?
+  	best
+  end
+
+  def make_best
+  	Answer.where(question: self.question, best: true).update_all(best: false)
+  	self.update_attributes(best: true)
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe Answer, type: :model do
 
   it { should belong_to(:user) }
   it { should validate_presence_of :user_id }
+
+  context "best answer" do
+    let!(:question) { create(:question) }
+
+    it "sets best answer" do
+      answer = create(:answer)
+      expect(answer).to_not be_best
+
+      answer.make_best
+      expect(answer).to be_best
+    end
+
+    it "sets only one best answer" do
+      answer1, answer2, answer3 = create_list(:answer, 3, question: question)
+
+      answer1.make_best
+      answer2.make_best
+
+      [answer1, answer2, answer3].each { |a| a.reload }
+
+      expect(answer1).to_not be_best
+      expect(answer2).to be_best
+      expect(answer3).to_not be_best
+    end
+  end
 end
