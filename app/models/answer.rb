7e78e9a6e08abbf769b26f39d65a65f8a5df1ec7
Sites Make <sitesmake@gmail.com>
2016-02-20 12:13:06 +0300

    Attachable question/answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachable
+
+  accepts_nested_attributes_for :attachments
 
   validates :body, presence: true
   validates :question_id, numericality: true, presence: true
