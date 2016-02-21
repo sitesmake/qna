de18374002646b571b103c1840c061a4775ac5de
Sites Make <sitesmake@gmail.com>
2016-02-21 23:46:28 +0300

    destroy attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,9 +1,22 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :load_attachment
+  before_action :check_attachment_author
 
   def destroy
-  	@attachment = Attachment.find(params[:id])
   	@attachment.destroy if current_user.author_of?(@attachment.attachable)
   	respond_to :js
+  end
+
+  private
+
+  def load_attachment
+    @attachment = Attachment.find(params[:id])
+  end
+
+  def check_attachment_author
+    unless current_user.author_of?(@attachment.attachable)
+      head(:forbidden)
+    end
   end
 end
--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,12 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe AttachmentsController, type: :controller do
+	let!(:user) { create(:user) }
+	let!(:question) { create(:question, user: user) }
+  let!(:attachment) { create(:attachment, attachable: question)}
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+	before { login(user) }
+
+	describe 'DELETE #destroy' do
+	  context 'with correct user' do
+	    it 'deletes answer' do
+	      expect { delete :destroy, id: attachment, format: :js }.to change(Attachment, :count).by(-1)
+	    end
+	  end
+
+	  context 'with incorrect user' do
+	    let(:incorrect_user) { create(:user) }
+
+	    before { login(incorrect_user) }
+
+	    it 'does not allow to destroy' do
+	      expect { delete :destroy, id: attachment, format: :js }.not_to change(Attachment, :count)
+	    end
+
+	    it 'returns forbidden' do
+	      delete :destroy, id: attachment
+	      expect(response).to be_forbidden
+	    end
+	  end
+	end
 
 end
