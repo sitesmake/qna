a4023234c0adfa4b8ebf18e4a103f8959c0debcb
Sites Make <sitesmake@gmail.com>
2016-03-05 18:50:50 +0300

    votable concern

---
+++ app/models/concerns/votable.rb
@@ -0,0 +1,19 @@
+module Votable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :votable
+  end
+
+  def rating
+    votes.sum(:voice)
+  end
+
+  def vote_up(user)
+    votes.create(voice: 1, user: user)
+  end
+
+  def vote_down(user)
+    votes.create(voice: -1, user: user)
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ActiveRecord::Base
+  include Votable
+
 	has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachable
-  has_many :votes, as: :votable
   belongs_to :user
 
   accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
@@ -9,15 +10,4 @@ class Question < ActiveRecord::Base
   validates :title, :body, presence: true
   validates :user_id, numericality: true, presence: true
 
-  def rating
-    self.votes.sum(:voice)
-  end
-
-  def vote_up(user)
-    self.votes.create(voice: 1, user: user)
-  end
-
-  def vote_down(user)
-    self.votes.create(voice: -1, user: user)
-  end
 end
