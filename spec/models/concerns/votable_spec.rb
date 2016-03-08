ba223537d97b3065b74540f66649c7d18046c514
Sites Make <sitesmake@gmail.com>
2016-03-08 09:23:04 +0300

    not reserved word

--- spec/models/concerns/votable_spec.rb
+++ spec/models/concerns/votable_spec.rb
@@ -6,23 +6,23 @@ shared_examples_for "votable" do
   it { should have_many :votes }
 
   context "votes methods" do
-    let!(:objekt) { create(model.to_s.underscore.to_sym) }
+    let!(:object) { create(model.to_s.underscore.to_sym) }
 
     it "can view rating" do
-      objekt.votes.create(voice: 1)
-      expect(objekt.rating).to eq(1)
+      object.votes.create(voice: 1)
+      expect(object.rating).to eq(1)
     end
 
     it "can vote up" do
       user = create(:user)
-      objekt.vote_up(user)
-      expect(objekt.rating).to eq(1)
+      object.vote_up(user)
+      expect(object.rating).to eq(1)
     end
 
     it "can vote down" do
       user = create(:user)
-      objekt.vote_down(user)
-      expect(objekt.rating).to eq(-1)
+      object.vote_down(user)
+      expect(object.rating).to eq(-1)
     end
   end
 
