a24c01f72d77e293429ee161eec96ea124f11005
Sites Make <sitesmake@gmail.com>
2016-03-08 08:35:00 +0300

    optimized query

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :questions
   has_many :answers
+  has_many :votes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,6 +12,7 @@ class User < ActiveRecord::Base
   end
 
   def voted_for?(model)
-    model.votes.collect(&:user_id).include?(self.id)
+    #model.votes.collect(&:user_id).include?(self.id)
+    votes.where(votable: model).exists?
   end
 end
