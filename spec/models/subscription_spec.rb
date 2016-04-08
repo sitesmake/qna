70e1b7ca549e5bb9cce5595c08709541d25dd01c
Sites Make <sitesmake@gmail.com>
2016-04-08 19:22:52 +0300

    subscription model

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  subject { create(:subscription) }
+
+  it { should belong_to(:question) }
+  it { should belong_to(:user) }
+
+  it { should validate_presence_of :user_id }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:question_id) }
+
+  it { should validate_presence_of :question_id }
+end
