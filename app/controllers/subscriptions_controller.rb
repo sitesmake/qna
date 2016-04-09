543e65176ef039cf8deeb35a27373a225e69acf3
Sites Make <sitesmake@gmail.com>
2016-04-09 17:09:01 +0300

    one method

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,28 +1,18 @@
 class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_question, only: :create
-  before_action :load_subscription, only: :destroy
 
   respond_to :js
 
   authorize_resource
 
   def create
+    @question = Question.find(params[:question_id])
     respond_with(current_user.create_subscription(@question))
   end
 
   def destroy
+    @subscription = Subscription.find(params[:id])
     @question = @subscription.question
     respond_with(@subscription.destroy)
-  end
-
-  private
-
-  def load_question
-    @question = Question.find(params[:question_id])
-  end
-
-  def load_subscription
-    @subscription = Subscription.find(params[:id])
   end
 end
