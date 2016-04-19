ee6aa1e11e57aca25e12540aca355a520ec1e2ae
Sites Make <sitesmake@gmail.com>
2016-04-19 13:37:04 +0300

    cap deploy

--- Capfile
+++ Capfile
@@ -7,6 +7,7 @@ require 'capistrano/rvm'
 require 'capistrano/bundler'
 require 'capistrano/rails'
 require 'capistrano/sidekiq'
+require 'whenever/capistrano'
 
 # Include tasks from other gems included in your Gemfile
 #
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/home/deployer/qna'
 set :deploy_user, 'deployer'
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', '.env', 'config/private_pub_thin.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -24,4 +24,39 @@ namespace :deploy do
   end
 
   after :publishing, :restart
+end
+
+namespace :private_pub do
+  desc 'start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub.yml start"
+        end
+      end
+    end
+  end
+
+  desc 'stop private_pub server'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub.yml stop"
+        end
+      end
+    end
+  end
+
+  desc 'restart private_pub server'
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub.yml restart"
+        end
+      end
+    end
+  end
 end
